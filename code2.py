22b040758332bf3371af237564ae03d3176dfefe
Gabriela Mininel Valle <gabrieladvmininel@gmail.com>
2017-01-13 16:04:02 +0000

    Finalizacao da primeira versao oficial do meu portfolio. Fixed: setas, cores, planos de fundo, atalhos, links, paginas, thumbnails, display das imagens e galerias.

--- code2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-my_file = open('html3.html', 'r+')
-
-for img in range(10, 34):
-    my_file.write(
-        "<div class='col-lg-3 text-left'>\n<a href='img/portfolio/planodevoo/{}.jpg' class='highslide' onclick='return hs.expand(this)'>\n<img class='pdv-thumbnail' src='img/portfolio/planodevoo/{}.jpg' title='Click to enlarge'/></a></div>".format(
-            (str(img)), (str(img))))
-
-my_file.close()
